2233c34361027210231e5c7e00aa8093796353ac
n24008 <n24008@std.it-college.ac jp>
2024-07-24 08:01:23 +0000

    now

--- kadai1.py
+++
@@ -1,7 +0,0 @@
-stack = []
-stack.append(1)
-print(stack)
-stack.append(2)
-print(stack)
-print(stack.pop())
-print(stack.pop())
--- kadai2.py
+++
@@ -1,7 +0,0 @@
-qu = []
-qu.append(1)
-print(qu)
-qu.append(2)
-print(qu)
-print(qu.pop(0))
-print(qu.pop(0))
