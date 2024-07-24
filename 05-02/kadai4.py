c3971d08934df65282f5faa3c126f54a059bdb3b
n24008 <n24008@std.it-college.ac jp>
2024-07-24 08:13:39 +0000

    7,24

---
+++ 05-02/kadai4.py
@@ -0,0 +1,5 @@
+list1 = [1,2,3,4,5,6,7,8,9]
+print([x if x%2==0 else None for x in list1])
+
+#③ [None, 2, None, 4, None, 6, None, 8, None]である
+
---
+++ 05-02/kadai5.py
@@ -0,0 +1,5 @@
+print([x * y for x in range(3) for y in range(x+1)])
+
+#③ [0, 0, 1, 0, 2, 4]である
+
+
