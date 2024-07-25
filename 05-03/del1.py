48db84f714406decf15d83d0c283f28a0406bf5b
n24008 <n24008@std.it-college.ac jp>
2024-07-25 08:00:15 +0000

    7,25

---
+++ 05-03/del1.py
@@ -0,0 +1,6 @@
+members = ['Yamada','Satou','Suzuki','Tanaka','Itou']
+del members[2]
+print(members)
+
+#② ['Yamada', 'Satou', 'Tanaka', 'Itou']である
+
---
+++ 05-04/tapulu1.py
@@ -0,0 +1,5 @@
+a, b, c, d, e = ('Sapporo','Sendai','Tokyo','Nagoya','Osaka')
+g = a + e 
+print(g)
+
+#③ SapporoOsakaである
