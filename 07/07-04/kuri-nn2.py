633e1b9def1ac0dc9d50cdf9063e646b4ae11e4d
n24008 <n24008@std.it-college.ac jp>
2024-08-20 07:09:36 +0000

    8,20

---
+++ 07/07-04/kuri-nn2.py
@@ -0,0 +1,16 @@
+def test_function():
+    try:
+        print('try')
+        return
+    except:
+        print('except')
+    else:
+        print('else')
+    finally:
+        print('finally')
+
+test_function()
+
+#try
+#finally
+#① である
