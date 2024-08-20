9c755bfa9673553e0b5081c5fed1cc4c22aea110
n24008 <n24008@std.it-college.ac jp>
2024-08-20 06:23:25 +0000

    8,20

---
+++ 07/07-01/erorr3.py
@@ -0,0 +1,5 @@
+int('3.5')
+
+#ValueError: invalid literal for int() with base 10: '3.5'
+#④ です
+
---
+++ 07/07-01/erorr4.py
@@ -0,0 +1,5 @@
+my_list = []
+my_list.insert(3)
+
+#TypeError: insert expected 2 arguments, got 1
+#③ である
